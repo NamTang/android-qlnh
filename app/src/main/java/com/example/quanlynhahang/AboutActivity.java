82d01e505fe419feff3265dc24022754ce06c394
NamTang <namkty96@gmail.com>
2019-06-10 18:06:44 +0700

    add code from team member - call;

--- app/src/main/java/com/example/quanlynhahang/AboutActivity.java
+++ app/src/main/java/com/example/quanlynhahang/AboutActivity.java
@@ -1,47 +1,104 @@
 package com.example.quanlynhahang;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    TextView txtPhone1, txtPhone2, txtPhone3, txtPhone4, txtPhone5, txtPhone6, txtPhone7;
+    ImageView imgPhone1, imgPhone2, imgPhone3, imgPhone4, imgPhone5, imgPhone6, imgPhone7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        initActionBar();
+        addControls();
+        addEvents();
     }
 
-    private void initActionBar() {
-        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setDisplayShowCustomEnabled(true);
-        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
-        View view = getSupportActionBar().getCustomView();
-        ImageView imgBack = view.findViewById(R.id.imgBack);
-        imgBack.setOnClickListener(new View.OnClickListener() {
+    private void addEvents() {
+        imgPhone1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                String num = txtPhone1.getText().toString();
+                call(num);
+            }
+        });
+        imgPhone2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num = txtPhone2.getText().toString();
+                call(num);
+            }
+        });
+        imgPhone3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num = txtPhone3.getText().toString();
+                call(num);
+            }
+        });
+        imgPhone4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num = txtPhone4.getText().toString();
+                call(num);
+            }
+        });
+        imgPhone5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num = txtPhone5.getText().toString();
+                call(num);
             }
         });
 
-        // menu button
-        ImageView imgMenu = view.findViewById(R.id.imgMenu);
-        imgMenu.setOnClickListener(new View.OnClickListener() {
+        imgPhone6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                goMenu();
+                String num = txtPhone6.getText().toString();
+                call(num);
+            }
+        });
+        imgPhone7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num = txtPhone7.getText().toString();
+                call(num);
             }
         });
     }
 
-    private void goMenu() {
-        Intent menu = new Intent(this, MenuActivity.class);
-        menu.putExtra("from", "table");
-        startActivity(menu);
+    private void call(String number) {
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel: " + number));
+        startActivity(callIntent);
+    }
+
+    private void addControls() {
+        txtPhone1 = findViewById(R.id.txtPhone1);
+        txtPhone2 = findViewById(R.id.txtPhone2);
+        txtPhone3 = findViewById(R.id.txtPhone3);
+        imgPhone1 = findViewById(R.id.imgPhone1);
+        imgPhone2 = findViewById(R.id.imgPhone2);
+        imgPhone3 = findViewById(R.id.imgPhone3);
+
+        txtPhone4 = findViewById(R.id.txtPhone4);
+        imgPhone4 = findViewById(R.id.imgPhone4);
+
+        txtPhone5 = findViewById(R.id.txtPhone5);
+        imgPhone5 = findViewById(R.id.imgPhone5);
+
+        txtPhone6 = findViewById(R.id.txtPhone6);
+        imgPhone6 = findViewById(R.id.imgPhone6);
+        txtPhone7 = findViewById(R.id.txtPhone7);
+        imgPhone7 = findViewById(R.id.imgPhone7);
+
     }
 }
